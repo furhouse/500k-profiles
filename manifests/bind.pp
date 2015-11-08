92eb9bf9e24659374be79c7398a603a42f9a7058
Wim <wim@500k.nl>
2015-11-08 15:42:27 +0100

    added wrapper for every bind defined type and resource_record

--- manifests/bind.pp
+++ manifests/bind.pp
@@ -1,14 +1,18 @@
 class profiles::bind {
 
-  $bindacl = hiera('profiles::bind::bindacl')
+  $bindkey  = hiera('profiles::bind::bindkey')
+  $bindacl  = hiera('profiles::bind::bindacl')
+  $bindzone = hiera('profiles::bind::bindzone')
+  $bindview = hiera('profiles::bind::bindview')
+  $bindrr   = hiera('profiles::bind::bindrr')
 
   contain '::bind'
 
+  create_resources('bind::key', $bindkey)
   create_resources('bind::acl', $bindacl)
-
-  # create_resources('dns::zone', $dnszone)
-  # create_resources('dns::zone', $dnszone)
-  # create_resources('dns::zone', $dnszone)
+  create_resources('bind::zone', $bindzone)
+  create_resources('bind::view', $bindview)
+  create_resources('resource_record', $bindrr)
 
 }
 
