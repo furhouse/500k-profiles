afe8d4770cd2dc9289e741b2bd8196eede4d4df9
Wim <wim@500k.nl>
2015-12-04 00:00:16 +0100

    added ssl dir

--- manifests/foremanproxy.pp
+++ manifests/foremanproxy.pp
@@ -2,6 +2,13 @@ class profiles::foremanproxy {
 
   ## SSL and CA configuration
   # Open read permissions to private keys to puppet group for foreman, proxy etc.
+  file { "${::puppet_vardir}/ssl":
+    ensure => directory,
+    owner  => $puppet::server_user,
+    group  => $puppet::server_group,
+    mode   => '0750',
+  }
+
   file { "${::puppet_vardir}/ssl/private_keys":
     ensure => directory,
     owner  => $puppet::server_user,
