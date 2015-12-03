ffa1f1c64fbbb3cd97b4a30c0784a4703b9dbfb9
Wim <wim@500k.nl>
2015-12-03 23:55:29 +0100

    profile foremanproxy as wrapper for cert permissions

---
+++ manifests/foremanproxy.pp
@@ -0,0 +1,18 @@
+class profiles::foremanproxy {
+
+  ## SSL and CA configuration
+  # Open read permissions to private keys to puppet group for foreman, proxy etc.
+  file { "${::puppet_vardir}/ssl/private_keys":
+    ensure => directory,
+    owner  => $puppet::server_user,
+    group  => $puppet::server_group,
+    mode   => '0750',
+  }
+
+  file { "${::puppet_vardir}/ssl/private_keys/${::fqdn}.pem":
+    owner => $puppet::server_user,
+    group => $puppet::server_group,
+    mode  => '0640',
+  }
+
+}
