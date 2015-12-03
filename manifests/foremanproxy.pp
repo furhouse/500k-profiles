a3f4b0dfb98eeb2b06a7ff4361b555f019ad5fa9
Wim <wim@500k.nl>
2015-12-04 00:26:57 +0100

    cheated with owner & group

--- manifests/foremanproxy.pp
+++ manifests/foremanproxy.pp
@@ -2,26 +2,23 @@ class profiles::foremanproxy {
 
   ## SSL and CA configuration
   # Open read permissions to private keys to puppet group for foreman, proxy etc.
-
-  contain '::puppet'
-
   file { "${::puppet_vardir}/ssl":
     ensure => directory,
-    owner  => $puppet::server_user,
-    group  => $puppet::server_group,
+    owner  => puppet,
+    group  => puppet,
     mode   => '0750',
   }
 
   file { "${::puppet_vardir}/ssl/private_keys":
     ensure => directory,
-    owner  => $puppet::server_user,
-    group  => $puppet::server_group,
+    owner  => puppet,
+    group  => puppet,
     mode   => '0750',
   }
 
   file { "${::puppet_vardir}/ssl/private_keys/${::fqdn}.pem":
-    owner => $puppet::server_user,
-    group => $puppet::server_group,
+    owner => puppet,
+    group => puppet,
     mode  => '0640',
   }
 
