caa13ad730d4ec7723844eab7ad4bee0a31904a5
Wim <wim@500k.nl>
2016-01-31 15:52:37 +0100

    removed permission from ssl dir

--- manifests/foremanproxy.pp
+++ manifests/foremanproxy.pp
@@ -2,12 +2,12 @@ class profiles::foremanproxy {
 
   ## SSL and CA configuration
   # Open read permissions to private keys to puppet group for foreman, proxy etc.
-  file { "${::puppet_vardir}/ssl":
-    ensure => directory,
-    owner  => puppet,
-    group  => puppet,
-    mode   => '0750',
-  }
+  # file { "${::puppet_vardir}/ssl":
+    # ensure => directory,
+    # owner  => puppet,
+    # group  => puppet,
+    # mode   => '0750',
+  # }
 
   file { "${::puppet_vardir}/ssl/private_keys":
     ensure => directory,
