16feb1e4239312577a018b6a0c4106c5942e1e84
Wim <wim@500k.nl>
2016-01-31 23:05:45 +0100

    removed contain

--- manifests/samba.pp
+++ manifests/samba.pp
@@ -4,16 +4,12 @@ class profiles::samba {
 
   $share  = hiera('profiles::samba::share')
 
-  # contain '::samba'
-
   class { 'samba::server':
     workgroup     => '500k.lan',
     server_string => '500k.lan Samba',
     interfaces    => 'br0 virbr0',
     security      => 'share',
   }
-
-  contain '::samba::server::share'
 
   create_resources(samba::server::share, $share)
 
