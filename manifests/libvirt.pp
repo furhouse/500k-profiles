013d5dcb1138a6407c0c9640b58d871a8417ff6a
Wim <wim@500k.nl>
2016-01-31 17:54:57 +0100

    added pool switch & dir

--- manifests/libvirt.pp
+++ manifests/libvirt.pp
@@ -6,4 +6,11 @@ class profiles::libvirt {
 
   create_resources('libvirt::pool', $pool)
 
+  if $pool {
+    file { 'libvirt_pool':
+      ensure => directory,
+      path   => $pool,
+    }
+  }
+
 }
