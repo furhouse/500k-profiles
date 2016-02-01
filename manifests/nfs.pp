50651b91a5c4b92aac1d06dfe8591004397cafb8
Wim <wim@500k.nl>
2016-02-01 22:44:01 +0100

    removed mount_root param

--- manifests/nfs.pp
+++ manifests/nfs.pp
@@ -5,17 +5,12 @@ class profiles::nfs {
   $nfs_v4_export_root         = hiera('profiles::nfs::nfs_v4_export_root')
   $nfs_v4_export_root_clients = hiera('profiles::nfs::nfs_v4_export_root_clients')
   $nfs_v4_idmap_domain        = hiera('profiles::nfs::nfs_v4_idmap_domain')
-  $nfs_v4_mount_root          = hiera('profiles::nfs::nfs_v4_mount_root')
 
   class { 'nfs::server':
     nfs_v4                     => $nfs_v4,
     nfs_v4_export_root         => $nfs_v4_export_root,
     nfs_v4_export_root_clients => $nfs_v4_export_root_clients,
     nfs_v4_idmap_domain        => $nfs_v4_idmap_domain,
-  }
-
-  class { 'nfs::params':
-    nfs_v4_mount_root => $nfs_v4_mount_root,
   }
 
   create_resources(nfs::server::export, $export)
