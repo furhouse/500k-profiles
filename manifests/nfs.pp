5c11ec79ae00107328d1562757eda4f4d0650094
Wim <wim@500k.nl>
2016-02-01 22:13:54 +0100

    testing nfs module wrapper

---
+++ manifests/nfs.pp
@@ -0,0 +1,20 @@
+class profiles::nfs {
+
+  $export                     = hiera('profiles::nfs::export')
+  $nfs_v4                     = hiera('profiles::nfs::nfs_v4')
+  $nfs_v4_export_root         = hiera('profiles::nfs::nfs_v4_export_root')
+  $nfs_v4_export_root_clients = hiera('profiles::nfs::nfs_v4_export_root_clients')
+  $nfs_v4_mount_root          = hiera('profiles::nfs::nfs_v4_mount_root')
+  $nfs_v4_idmap_domain        = hiera('profiles::nfs::nfs_v4_idmap_domain')
+
+  class { 'nfs::server':
+    nfs_v4                     => $nfs_v4,
+    nfs_v4_export_root         => $nfs_v4_export_root,
+    nfs_v4_export_root_clients => $nfs_v4_export_root_clients,
+    nfs_v4_mount_root          => $nfs_v4_mount_root,
+    nfs_v4_idmap_domain        => $nfs_v4_idmap_domain,
+  }
+
+  create_resources(nfs::server::export, $export)
+
+}
