93a980dcff3489545ff0de8b9dea1c2e635f5aaf
Wim <wim@500k.nl>
2016-03-05 22:40:51 +0100

    again vhost mgmt by apache module, also removed localhost conf

--- manifests/backuppcserver.pp
+++ manifests/backuppcserver.pp
@@ -1,16 +1,29 @@
-class profiles::backuppcserver {
+class profiles::backuppcserver inherits ::backuppc::params {
 
   $serverpass = hiera('profiles::apache::serverpass')
 
-  class { '::apache': } ->
+  class { '::apache': default_vhost => false }
+
+  apache::vhost { $::fqdn:
+    docroot        => $::backuppc::params::cgi_directory,
+    manage_docroot => false,
+    port           => '80',
+    aliases        => [ { alias => '/backuppc', path => $::backuppc::params::cgi_directory, }, ],
+    directories    => [
+      { path           => $::backuppc::params::cgi_directory,
+        directoryindex => 'index.cgi',
+        options        => ['ExecCGI','FollowSymLinks'],
+        addhandlers    => [ { handler => 'cgi-script', extensions => ['.cgi'] }, ],
+        auth_user_file => $::backuppc::params::htpasswd_apache,
+        auth_type      => 'basic',
+        auth_name      => 'BackupPC admin',
+        auth_require   => 'valid-user',
+      },
+    ],
+  }
 
   package { 'apache2-utils':
     ensure  => present,
-  } ->
-
-  class { '::backuppc::server':
-    backuppc_password => $serverpass,
-    notify            => Service['apache2'],
   }
 
   class { '::backuppc::client':
@@ -22,6 +35,26 @@ class profiles::backuppcserver {
     tar_incr_args     => '--newer=$incrDate $fileList',
   }
 
+  backuppc::server::user { 'backuppc':
+    password => $serverpass,
+    require  => Package['apache2-utils'],
+  }
+
+  class { '::backuppc::server':
+    backuppc_password    => $serverpass,
+    apache_configuration => false,
+  } ->
+
+  exec { 'remove-localhost-hosts':
+    command => "/bin/sed -i '/localhost/ d' ${::backuppc::params::hosts}",
+    onlyif  => "/bin/grep localhost ${::backuppc::params::hosts}",
+  }
+
+  file { 'remove-localhost-config':
+    ensure => absent,
+    path   => "${::backuppc::params::config_directory}/localhost.pl",
+  }
+
   file { '/etc/sudoers.d/backuppc_localhost':
     ensure  => file,
     owner   => 'root',
@@ -29,4 +62,5 @@ class profiles::backuppcserver {
     mode    => '0440',
     content => "backuppc ALL=(ALL:ALL) NOEXEC:NOPASSWD: /bin/tar\n",
   }
+
 }
