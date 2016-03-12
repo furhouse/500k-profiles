2176cfc99ee80c782c5beeb40c47e86d867d7a0b
Wim Bonthuis <wim@500k.nl>
2016-03-12 14:24:10 +0100

    1 subdir too many

--- manifests/postfix.pp
+++ manifests/postfix.pp
@@ -18,7 +18,7 @@ class profiles::postfix {
 
   include ::staging
   staging::deploy { 'postfixadmin-2.93.tar.gz':
-    source => "puppet:///modules/${module_name}/files/postfixadmin/postfixadmin-2.93.tar.gz",
+    source => "puppet:///modules/${module_name}/files/postfixadmin-2.93.tar.gz",
     target => '/var/www/postfixadmin',
   }
 
