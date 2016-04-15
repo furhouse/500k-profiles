7c22bf04747820f955723313bedf13acd07fd1b8
Corinna von den Driesch <corinna.vondendriesch@takeaway.com>
2016-04-15 13:48:49 +0200

    added mysql db and user creation to manifest

--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -1,15 +1,17 @@
-class profiles::mysql {
+class profiles::mysql(
+  Hash $databases = [],
+) {
   
-  # Hiera lookups
   $mysql_root_password = hiera('profiles::mysql::mysql_root_password', 'somepassword')
-
-
-  # # Configure mysql
+  
   class { 'mysql::server':
     root_password => $mysql_root_password,
   }
-
+->
   class { 'mysql::bindings':
     php_enable => true,
   }
+  
+  create_resources('::mysql::db', $databases)
+  
 }
