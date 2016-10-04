4c7a814fb284c0858e362d2034c2b5b758e2fcf2
Wim Bonthuis <wim@500k.nl>
2016-10-04 18:11:16 +0200

    Moved backuppc client config to base.pp.

--- manifests/base.pp
+++ manifests/base.pp
@@ -12,4 +12,27 @@ class profiles::base {
     require => Class['::sudo'],
   }
 
+  $bpc_server = hiera('bpc_server', false)
+
+  if !$bpc_server {
+
+    class { '::backuppc::client':
+      backuppc_hostname    => hiera('bpc_host', undef),
+      rsync_share_name     => hiera_array('bpc_rsync_shares', []),
+      backup_files_exclude => hiera_hash('bpc_excludes', {}),
+    }
+  }
+
+  else {
+
+    class { '::backuppc::client':
+      backuppc_hostname => "${::fqdn}",
+      xfer_method       => 'tar',
+      tar_share_name    => [ '/home', '/etc', '/var/log'],
+      tar_client_cmd    => '/usr/bin/sudo $tarPath -c -v -f - -C $shareName --totals',
+      tar_full_args     => '$fileList',
+      tar_incr_args     => '--newer=$incrDate $fileList',
+    }
+  }
+
 }
