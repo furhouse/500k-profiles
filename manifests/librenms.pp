726afc29844dad799bd25280862bfe354b971aef
Wim <wim@500k.nl>
2017-03-28 00:42:18 +0200

    Added php5enmod mcrypt to librenms profile.

--- manifests/librenms.pp
+++ manifests/librenms.pp
@@ -1,12 +1,21 @@
 class profiles::librenms {
 
   # install some dependency packages (uses puppetlabs/stdlib)
-  ensure_packages(['php5-mysql', 'php5-mcrypt', 'php5-gd', 'php5-snmp', 'php-pear', 'python-mysqldb', 'php-net-ipv4', 'php-net-ipv6', 'rrdtool'])
+  ensure_packages(['php5-common', 'php5-mysql', 'php5-mcrypt', 'php5-gd', 'php5-snmp', 'php-pear', 'python-mysqldb', 'php-net-ipv4', 'php-net-ipv6', 'rrdtool'])
 
   # install librenms, config data is taken from hiera
   class { '::librenms':
     manage_git => false,
     mysql_pass => hiera('librenms_mysql_pass', undef)
+  }
+
+  # Ensure Mcrypt is enabled
+  exec { 'enable_mcrypt':
+    path    => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ],
+    command => 'php5enmod mcrypt',
+    notify  => Service['apache2'],
+    require => Package['php5-common'],
+    creates => '/etc/php5/apache2/conf.d/20-mcrypt.ini'
   }
 
   apache::vhost { 'librenms':
