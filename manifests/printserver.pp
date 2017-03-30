79d522c6749fd33bc347f89f32bc3a6a47883ed1
Wim <wim@500k.nl>
2017-03-30 15:01:26 +0200

    Created printserver profile.

--- manifests/printserver.pp
+++ manifests/printserver.pp
@@ -1,14 +1,14 @@
 class profiles::printserver {
 
   class { '::cups':
-    default_queue => hiera('cups_default_queue', undef),
+    default_queue => hiera('profiles::printserver::cups_default_queue', undef),
   }
 
   class { '::cups::server':
     port =>  631,
   }
 
-  $printers = hiera_hash('printers', {})
+  $printers = hiera_hash('profiles::printserver::printers', {})
   create_resources('cups_queue', $printers)
 
   package { 'cups-bsd':
@@ -16,9 +16,9 @@ class profiles::printserver {
   }
 
   class { '::samba::server':
-    workgroup     => hiera('smb_workgroup', undef),
-    server_string => hiera('smb_server_name', undef),
-    security      => hiera('smb_security', undef),
+    workgroup     => hiera('profiles::printserver::smb_workgroup', undef),
+    server_string => hiera('profiles::printserver::smb_server_name', undef),
+    security      => hiera('profiles::printserver::smb_security', undef),
     printing      => 'cups',
   }
 
