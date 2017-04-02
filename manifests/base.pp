c6279d3e042c8282de469aee01a8ce8fc99643cd
Wim <wim@500k.nl>
2017-04-02 19:09:53 +0200

    Moved lots of common.yaml stuff to base profile.

--- manifests/base.pp
+++ manifests/base.pp
@@ -2,14 +2,70 @@ class profiles::base {
 
   $packages = hiera_array('profiles::base::packages')
 
-  Package { ensure  => latest, }
-
-  package { $packages: }
+  package { $packages:
+    ensure => latest,
+  }
 
   group { 'sudo':
     ensure  => present,
     gid     => '27',
     require => Class['::sudo'],
+  }
+
+  $satellite = hiera('profiles::base::satellite_mail', true)
+  if $satellite {
+
+    class { '::postfix':
+      master_smtp         => 'smtp inet n - n - - smtpd',
+      myorigin            => hiera('profiles::base::satellite_origin', undef),
+      root_mail_recipient => hiera('profiles::base::satellite_root_rcpt', undef),
+    }
+
+    postfix::config {
+      'relayhost':                      value => hiera('profiles::base::satellite_relay', undef);
+      'smtp_sasl_auth_enable':          value => 'yes';
+      'smtp_sasl_password_maps':        value => 'hash:/etc/postfix/smtp_auth';
+      'smtp_sasl_security_options':     value => 'noanonymous';
+      'smtp_sasl_tls_security_options': value => 'noanonymous';
+    }
+
+    postfix::hash { '/etc/postfix/smtp_auth':
+      ensure  => 'present',
+      content => hiera('profiles::base::satellite_serv_creds', undef),
+    }
+  }
+
+  class { '::users': }
+  class { '::vim': }
+  class { '::git': }
+
+  class { '::sudo': }
+  $sudo_cfg = hiera('profiles::base::sudo_configs', {})
+  create_resources('::sudo::conf', $sudo_cfg)
+
+  class { '::timezone':
+    region   => hiera('profiles::base::tz_region', undef),
+    locality => hiera('profiles::base::tz_locality', undef),
+  }
+
+  class { '::locales':
+    default_locale => hiera('profiles::base::loc_default_locale', undef),
+    locales        => hiera_array('profiles::base::loc_locales', []),
+  }
+
+  class { '::ntp':
+    restrict => hiera_array('profiles::base::ntp_restrict', []),
+    servers  => hiera_array('profiles::base::ntp_servers', []),
+  }
+
+  class { '::ssh':
+    server_options => hiera_hash('profiles::base::ssh_server_options', {})
+  }
+
+  class { '::zabbix::agent':
+    server       => hiera('profiles::base::zbx_agent_server', undef),
+    serveractive => hiera('profiles::base::zbx_agent_serveractive', undef),
+    hostnameitem => hiera('profiles::base::zbx_agent_hostnameitem', undef),
   }
 
   class { '::gcs':
@@ -24,9 +80,9 @@ class profiles::base {
   if !$bpc_server {
 
     class { '::backuppc::client':
-      backuppc_hostname    => hiera('bpc_host', undef),
-      rsync_share_name     => hiera_array('bpc_rsync_shares', []),
-      backup_files_exclude => lookup('bpc_excludes', {}),
+      backuppc_hostname    => hiera('profiles::base::bpc_host', undef),
+      rsync_share_name     => hiera_array('profiles::base::bpc_rsync_shares', []),
+      backup_files_exclude => lookup('profiles::base::bpc_excludes', {}),
     }
   }
 
