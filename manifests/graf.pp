4033220f995391df3e01e538ff040a6286002175
Wim <wim@500k.nl>
2017-04-05 17:23:15 +0200

    Created grafana profile + updated mysql.

---
+++ manifests/graf.pp
@@ -0,0 +1,63 @@
+class profiles::grafana {
+
+  class { '::grafana':
+    cfg      => {
+      app_mode    => 'production',
+      server      => {
+        http_port => 8080,
+      },
+      database    => {
+        type     => 'mysql',
+        host     => '127.0.0.1:3306',
+        name     => hiera('profiles::grafana::db_name', undef),
+        user     => hiera('profiles::grafana::db_username', undef),
+        password => hiera('profiles::grafana::db_password', undef),
+      },
+      users       => {
+        allow_sign_up    => false,
+        allow_org_create => false,
+      },
+      snapshots   => {
+        external_enabled => false,
+      },
+      analytics   => {
+        reporting_enabled => false,
+      },
+      smtp        => {
+        enabled      => true,
+        host         => 'localhost:25',
+        from_address => "admin@${::fqdn}",
+        from_name    => 'Dashboard Admin'
+      },
+      'auth.ldap' => {
+        enabled     => true,
+        config_file => '/etc/grafana/ldap.toml',
+      },
+    },
+    ldap_cfg => {
+      servers => [
+        { host            => 'xxx',
+          use_ssl         => true,
+          search_filter   => '(sAMAccountName=%s)',
+          search_base_dns => [ 'xxx' ],
+          bind_dn         => 'xxx',
+          bind_password   => 'xxx',
+          port            => 3269+0,
+        },
+      ],
+      'servers.attributes' => {
+        name      => 'givenName',
+        surname   => 'sn',
+        username  => 'sAMAccountName',
+        member_of => 'memberOf',
+        email     => 'mail',
+      },
+      'servers.group_mappings' => [
+        { group_dn => 'xxx',
+          org_role => 'Viewers',
+        },
+      ],
+    }
+  }
+
+}
--- manifests/mysql.pp
+++ manifests/mysql.pp
@@ -1,19 +1,22 @@
-class profiles::mysql(
-) {
-  $databases = hiera_hash('profiles::mysql::databases')
-  
-  notify{"$databases":}
-  
-  $mysql_root_password = hiera('profiles::mysql::mysql_root_password', 'somepassword')
-  
-  class { 'mysql::server':
-    root_password => $mysql_root_password,
+class profiles::mysql {
+
+  class { '::mysql::server':
+    root_password           => hiera('profiles::mysql::mysql_rootpass', undef),
+    remove_default_accounts => true,
+    package_name            => hiera('profiles::mysql::mysql_version', 'mysql-server-5.6'),
+    override_options        => {
+      'mysqld' => {
+        'table_definition_cache' => '50',
+      },
+    }
   }
-->
-  class { 'mysql::bindings':
-    php_enable => true,
-  }
-  
-  create_resources('::mysql::db', $databases)
-  
+
+  $sqldatabases = hiera_hash('profiles::mysql::sqldatabases', {})
+  $sqlusers = hiera_hash('profiles::mysql::sqlusers', {})
+  $sqlgrants = hiera_hash('profiles::mysql::sqlgrants', {})
+
+  create_resources('mysql_database', $sqldatabases)
+  create_resources('mysql_user', $sqlusers)
+  create_resources('mysql_grant', $sqlgrants)
+
 }
