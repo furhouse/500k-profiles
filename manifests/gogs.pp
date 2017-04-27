6e20c48e25ceb33c2049c5dabffe8681320dc88f
Wim <wim@500k.nl>
2017-04-27 22:22:37 +0200

    Forked gogs module, set manage_packages to false.

--- manifests/gogs.pp
+++ manifests/gogs.pp
@@ -3,6 +3,7 @@ class profiles::gogs {
   class { '::gogs':
     app_ini          => hiera_hash('profiles::gogs::app_ini', {}),
     app_ini_sections => hiera_hash('profiles::gogs::app_ini_sections', {}),
+    manage_packages  => false,
   }
 
 }
