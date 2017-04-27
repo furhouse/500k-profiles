220e54f99d443b16a6830443c09f1ccdf14bc673
Wim <wim@500k.nl>
2017-04-27 21:03:38 +0200

    Created gogs profile wrapper.

---
+++ manifests/gogs.pp
@@ -0,0 +1,8 @@
+class profiles::gogs {
+
+  class { '::gogs':
+    app_ini          => hiera_hash('profiles::gogs::app_ini', {}),
+    app_ini_sections => hiera_hash('profiles::gogs::app_ini_sections', {}),
+  }
+
+}
